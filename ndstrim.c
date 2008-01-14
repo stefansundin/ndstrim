7e236094dd4144519cc5c77aad04c36b7bae079c
Stefan Sundin <stefansundin@users.noreply.github.com>
2008-01-14 20:28:17 +0000

    Added check for romsize == 0 in CLI. Put screenshots in rep.

--- ndstrim.c
+++ ndstrim.c
@@ -78,45 +78,55 @@ int main(int argc, char *argv[]) {
 	fseek(input,0x80,SEEK_SET);
 	struct uint32 romsize;
 	fread(&romsize,sizeof(romsize),1,input);
-	
-	//Check if rom is big enough to contain the rom
-	if (filesize < romsize.data) {
-		fprintf(stderr,"%s: Error: '%s' is too small to contain the whole rom (corrupt rom?).\n",argv[0],argv[1]);
-		exit(1);
-	}
-	
-	//Check if rom seems to have been trimmed before
-	if (filesize == romsize.data) {
-		fprintf(stderr,"%s: Warning: '%s' is the same size as the trimmed rom will be.\n",argv[0],argv[1]);
-		fprintf(stderr,"%s: Warning: Maybe the rom have already been trimmed?\n",argv[0]);
-	}
-	
-	//Check if rom have a wifi block
-	//Wifi data is located on 136 bytes after rom data
 	unsigned int saferomsize=romsize.data;
 	int wifi_block=0;
-	if (filesize >= romsize.data+136) {
-		//Read wifi_data from rom
-		char wifi_data[136];
-		fseek(input,romsize.data,SEEK_SET);
-		fread(wifi_data,1,sizeof(wifi_data),input);
-		//Compare with 0x00 and 0xFF
-		char wifi_compare[136];
-		memset(wifi_compare,0x00,136);
-		if (memcmp(wifi_data,wifi_compare,136) != 0) {
-			//wifi_data is NOT filled with 0x00
-			memset(wifi_compare,0xFF,136);
-			if (memcmp(wifi_data,wifi_compare,136) != 0) {
-				//wifi_data is NOT filled with 0xFF
-				//Since wifi_data doesn't consist of 0x00 or 0xFF, the rom contains a wifi block
-				wifi_block=1;
-				saferomsize+=136;
-			}
-		}
+	
+	//Check if romsize is 0 (some commercial roms use zero, unknown reason)
+	//These roms usually have the whole rom filled with stuff (unknown if it's garbage or useful stuff)
+	//Known roms: 0040, 0132, 0192, 0318, and 0357 have this (All these are Japanese roms)
+	if (romsize.data == 0) {
+		fprintf(stderr,"%s: Warning: '%s' header romsize is 0.\n",argv[0],argv[1]);
+		fprintf(stderr,"%s: Warning: This rom will be copied.\n",argv[0]);
+		saferomsize=filesize;
 	}
 	else {
-		fprintf(stderr,"%s: Warning: '%s' is too small to contain a wifi block.\n",argv[0],argv[1]);
-		fprintf(stderr,"%s: Warning: This shouldn't be a problem if the rom have been properly trimmed before.\n",argv[0]);
+		//Check if rom is big enough to contain the rom
+		if (filesize < romsize.data) {
+			fprintf(stderr,"%s: Error: '%s' is too small to contain the whole rom (corrupt rom?).\n",argv[0],argv[1]);
+			exit(1);
+		}
+		
+		//Check if rom seems to have been trimmed before
+		if (filesize == romsize.data) {
+			fprintf(stderr,"%s: Warning: '%s' is the same size as the trimmed rom will be.\n",argv[0],argv[1]);
+			fprintf(stderr,"%s: Warning: Maybe the rom have already been trimmed?\n",argv[0]);
+		}
+		
+		//Check if rom have a wifi block
+		//Wifi data is located on 136 bytes after rom data
+		if (filesize >= romsize.data+136) {
+			//Read wifi_data from rom
+			char wifi_data[136];
+			fseek(input,romsize.data,SEEK_SET);
+			fread(wifi_data,1,sizeof(wifi_data),input);
+			//Compare with 0x00 and 0xFF
+			char wifi_compare[136];
+			memset(wifi_compare,0x00,136);
+			if (memcmp(wifi_data,wifi_compare,136) != 0) {
+				//wifi_data is NOT filled with 0x00
+				memset(wifi_compare,0xFF,136);
+				if (memcmp(wifi_data,wifi_compare,136) != 0) {
+					//wifi_data is NOT filled with 0xFF
+					//Since wifi_data doesn't consist of 0x00 or 0xFF, the rom contains a wifi block
+					wifi_block=1;
+					saferomsize+=136;
+				}
+			}
+		}
+		else {
+			fprintf(stderr,"%s: Warning: '%s' is too small to contain a wifi block.\n",argv[0],argv[1]);
+			fprintf(stderr,"%s: Warning: This shouldn't be a problem if the rom have been properly trimmed before.\n",argv[0]);
+		}
 	}
 	
 	//Print info
